f9b3b8b609172d5a3e387b5eea63ea37f1853cdf
kimhyoungnam <hnking1004@naver.com>
2017-12-09 15:17:51 +0900

    ok

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.locals.querystring = require('querystring');
 // mongodb connect
 //=======================================================
 mongoose.Promise = global.Promise; // ES6 Native Promise를 mongoose에서 사용한다.
-const connStr =   'mongodb://admin:1234@ds133776.mlab.com:33776/ison';
+const connStr =   'mongodb://hyoungnam:hnking785@@ds133776.mlab.com:33776/ison';
 
 
 // 아래는 mLab을 사용하는 경우의 예: 본인의 접속 String으로 바꾸세요.
